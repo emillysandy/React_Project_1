b936c3466e08bc441aeb5d38b124ee404ca825f3
Emilly Sandy <emillysandy05@gmail.com>
2022-06-26 12:45:20 -0300

    feat: add assignment]

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -17,13 +17,33 @@ function App() {
     })))
   }
 
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Formulario setTarefas={setTarefas}/>
+      <Formulario setTarefas={setTarefas} />
       <Lista
         tarefas={tarefas}
-        selecionaTarefa={selecionaTarefa}/>
-      <Cronometro selecionado={selecionado} />
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
